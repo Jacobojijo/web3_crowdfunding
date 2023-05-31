f87d6eba8e5306716b8a8b9e427e6097bfa0bee8
Jacob Ojijo <92510794+Jacobojijo@users.noreply.github.com>
2023-05-31 18:56:22 +0300

    Update vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,13 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 8000,
+     watch: {
+       usePolling: true
+     }
+  },
   define: {
     global: "globalThis",
     "process.env": {},
